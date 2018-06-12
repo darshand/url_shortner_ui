234defbe1c9d8df31a20adc0015b892c4289c253
darshand <darshan.bd@onze.in>
2018-06-12 15:50:37 +0530

    post call to api server

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,9 +2,31 @@ class PageController < ApplicationController
 
   # show a form to submit long url
   def post_url
+    # take url from form
+    originial_url = "My/url/from/user/input"
+    # submit url to api server
+    @response = post_call(originial_url)
   end
 
   # form to get original url
   def redirect_url
+  end
+
+  private
+  
+  def post_call(data)
+    remote_url = "http://localhost:3000/url"
+    uri = URI(remote_url)
+    req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+    req.body = .to_json
+    response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+        http.request(req)
+    end
+    return JSON.parse(response.body)['short_url'] if response.code == '201'
+    'Failed to create short url'
+  end
+
+  def get_call
+    
   end
 end
